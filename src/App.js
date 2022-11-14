7fc677f9469eed2070cf4b0641a2fb7940ff3222
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-11-15 00:25:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Items from "./components/items";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <Items></Items>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/items.js
@@ -0,0 +1,10 @@
+
+function Items(){
+    return <h2> Food Rs 10
+
+	Petrol Rs 100
+
+	Movies Rs 200  </h2> 
+}
+
+export default Items;
