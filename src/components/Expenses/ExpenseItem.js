1402dd1b98cd54a78f77e6cc8d2edc9dfca26de6
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-12-03 15:32:55 +0530

    dom manipulation

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,23 @@ import ExpenseAmount from './ExpenseAmount';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    
+    const clickHandler = () => {
+        console.log("Clicked!!!");
+    }
+
+    const removeExpense = () => {
+        const element = document.getElementById('expense')
+        element.remove();
+        console.log("removed");
+    }
+
     return( 
-    <div className='expense-item'> 
+    <div className='expense-item' id='expense'> 
         <ExpenseDate date={props.date} />
         <ExpenseAmount amount = {props.amount} title={props.title} />
         <div> {props.LocationOfExpenditure}</div>
+        <button onClick={clickHandler}> Change Title</button>
+        <button onClick={removeExpense}> remove</button>
     </div>
     );
 };
