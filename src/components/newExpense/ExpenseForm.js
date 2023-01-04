42bc6f8b32a3a2e537ffca4fa6360027a3151d69
Himanshudevg2000 <himanshudevgade87@gmail.com>
2023-01-04 11:39:33 +0530

    update

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -1,16 +1,20 @@
-import react from "react";
+import React, { useState } from "react";
 
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+
+    const [enterTitle, setEnteredTitle] = useState('');
+    const [enterAmount, setEnteredAmount] = useState('');
+    const [enterDate, setEnteredDate] = useState('');
     const titleChangeHandler = (event) => {
-        console.log(event.target.value)
+        setEnteredTitle(event.target.value)
     };
     const amountChangeHandler = (event) => {
-        console.log(event.target.value)
+        setEnteredAmount(event.target.value)
     };
-    const DateChangeHandler = (event) => {
-        console.log(event.target.value)
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
     };
 
     return <form>
@@ -25,7 +29,7 @@ const ExpenseForm = () => {
             </div>
             <div className="new-expense__controls">
                 <label>Date</label>
-                <input type='date' min="2023-01-02" onChange={DateChangeHandler}/>
+                <input type='date' min="2023-01-02" onChange={dateChangeHandler}/>
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
